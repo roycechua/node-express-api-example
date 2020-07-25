c233f1c121710aa53081bb68a3d1574e4133b40f
roycechua23 <royce236@gmail.com>
2020-07-25 23:37:52 +0800

    add require for jsonwebtoken library

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
