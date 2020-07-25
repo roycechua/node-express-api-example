b075dd4c4a31c778aee09a82b99f2fb4f108b36a
roycechua23 <royce236@gmail.com>
2020-07-26 01:22:02 +0800

    Fix line position of res.send in post /tracks

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -26,10 +26,10 @@ router.post('/tracks', async (req, res) => {
     try {
         const track = new Track({ name, locations, userId: req.user._id });
         await track.save();
+        res.send(track);
     } catch (err) {
         res.status(422).send({ error: err.message });
     }
-    res.send(track);
 });
 
 module.exports = router;
